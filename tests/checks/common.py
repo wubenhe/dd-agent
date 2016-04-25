77a11b66c8ac56d117a987296d19ab96696e1422
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2016-04-25 11:19:49 -0400

    [sdk] use environment variable instead of attribute to modify SDK testing check loading behavior.

--- tests/checks/common.py
+++ tests/checks/common.py
@@ -61,7 +61,7 @@ def load_class(check_name, class_name):
     raise Exception(u"Unable to import class {0} from the check module.".format(class_name))
 
 
-def load_check(name, config, agentConfig, is_sdk=None):
+def load_check(name, config, agentConfig, is_sdk=False):
     if not is_sdk:
         checksd_path = get_checksd_path(get_os())
 
@@ -140,17 +140,17 @@ class AgentCheckTest(unittest.TestCase):
         if not hasattr(self, 'CHECK_NAME'):
             raise Exception("You must define CHECK_NAME")
 
-        if not hasattr(self, 'SDK'):
-            self.SDK = False
-
         self.check = None
+
+    def is_sdk(self):
+        return "SDK_TESTING" in os.environ
 
     def is_travis(self):
         return "TRAVIS" in os.environ
 
     def load_check(self, config, agent_config=None):
         agent_config = agent_config or self.DEFAULT_AGENT_CONFIG
-        self.check = load_check(self.CHECK_NAME, config, agent_config, is_sdk=self.SDK)
+        self.check = load_check(self.CHECK_NAME, config, agent_config, is_sdk=self.is_sdk())
 
     def load_class(self, name):
         """
